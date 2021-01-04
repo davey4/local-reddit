16add8130f168684a71e682fa0f2eb9c3f837207
David Martel <daveymartel@gmail.com>
2021-01-04 09:58:10 -0500

    middleware auth

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
 const AppRouter = require("./routes/AppRouter");
-const express = require("express");
 const PORT = process.env.PORT || 3001;
+const express = require("express");
 const app = express();
+
+const path = require("path");
 
 // Require Middleware
 const logger = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const helmet = require("helmet");
 // Require Middleware
 
-// Initialize Middlewarenpx dt
+// Initialize Middleware
 app.use(logger("dev"));
+app.use(helmet({ contentSecurityPolicy: false }));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, "client", "build")));
 // Initialize Middleware
 
-app.get("/", (req, res) => res.send({ msg: "Server Working" }));
+app.disable("X-Powered-By");
+
+app.get("/", (req, res) => res.send({ msg: "Petsagram Server Working" }));
 app.use("/api", AppRouter);
 
-app.listen(PORT, () => console.log(`Serving listening on Port ${PORT}`));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+);
+
+app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`));
