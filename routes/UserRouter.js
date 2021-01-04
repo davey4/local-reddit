479510e46986858ed7cadadfe062284dffff15d1
David Martel <daveymartel@gmail.com>
2021-01-04 10:57:49 -0500

    login routes

---
+++ routes/UserRouter.js
@@ -0,0 +1,9 @@
+const Router = require("express").Router();
+const controller = require("../controllers/UserController");
+const { readToken, verifyJwt } = require("../middleware");
+
+Router.post("/register", controller.CreateUser);
+Router.post("/login", controller.LoginUser);
+Router.get("/refresh/session", readToken, verifyJwt, controller.RefreshSession);
+
+module.exports = Router;
